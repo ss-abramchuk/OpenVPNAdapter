9e3a72e02a92e6d22f44d9d41b8d60365af2680c
Sergey Abramchuk <ss.abramchuk@gmail.com>
2017-02-11 16:26:58 +0300

    Declare adapter delegate

--- OpenVPN Tunnel Provider/OpenVPNAdapter+Provider.h
+++ OpenVPN Tunnel Provider/OpenVPNAdapter+Provider.h
@@ -6,9 +6,32 @@
 //
 //
 
+#import "OpenVPNEvent.h"
+
 #import "OpenVPNAdapter.h"
+
+@class NEPacketTunnelFlow;
+@class NEPacketTunnelNetworkSettings;
+
+
+@protocol OpenVPNAdapterDelegate <NSObject>
+
+- (void)setTunnelSettings:(nonnull NEPacketTunnelNetworkSettings *)settings
+                 callback:(nonnull void (^)(NEPacketTunnelFlow * __nullable flow))callback
+NS_SWIFT_NAME(setTunnel(settings:callback:));
+
+- (void)handleEvent:(OpenVPNEvent)event
+            message:(nullable NSString *)message
+NS_SWIFT_NAME(handle(event:message:));
+
+- (void)handleError:(nonnull NSError *)error
+NS_SWIFT_NAME(handle(error:));
+
+@end
 
 
 @interface OpenVPNAdapter (Provider)
+
+@property (weak, nonatomic, null_unspecified) id<OpenVPNAdapterDelegate> delegate;
 
 @end
