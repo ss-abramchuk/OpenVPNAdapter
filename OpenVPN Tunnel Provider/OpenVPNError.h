5d24f03ad837e3531800c59ba1f55fb18ba29b1e
Sergey Abramchuk <ss.abramchuk@gmail.com>
2017-02-11 21:04:56 +0300

    Define error domain and key for fatal error

---
+++ OpenVPN Tunnel Provider/OpenVPNError.h
@@ -0,0 +1,12 @@
+//
+//  OpenVPNError.h
+//  OpenVPN iOS Client
+//
+//  Created by Sergey Abramchuk on 11.02.17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * __nonnull const OpenVPNClientErrorDomain;
+extern NSString *__nonnull const OpenVPNClientErrorFatalKey;
