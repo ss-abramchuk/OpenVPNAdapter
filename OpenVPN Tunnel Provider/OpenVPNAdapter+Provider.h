b6341f3ce92893c2fa6e272bbdfa56bda5ae7f7c
Sergey Abramchuk <ss.abramchuk@gmail.com>
2017-02-11 15:26:50 +0300

    Declare category for open vpn adapter so it will available for tunnel  provider

---
+++ OpenVPN Tunnel Provider/OpenVPNAdapter+Provider.h
@@ -0,0 +1,14 @@
+//
+//  OpenVPNAdapter+Provider.h
+//  OpenVPN iOS Client
+//
+//  Created by Sergey Abramchuk on 11.02.17.
+//
+//
+
+#import "OpenVPNAdapter.h"
+
+
+@interface OpenVPNAdapter (Provider)
+
+@end
