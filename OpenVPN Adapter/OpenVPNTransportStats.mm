d69f6e8e32dd5d46362dea69464ba9171786ba46
Sergey Abramchuk <ss.abramchuk@gmail.com>
2017-04-26 14:15:17 +0300

    Wrap transport stats

---
+++ OpenVPN Adapter/OpenVPNTransportStats.mm
@@ -0,0 +1,29 @@
+//
+//  OpenVPNTransportStats.m
+//  OpenVPN Adapter
+//
+//  Created by Sergey Abramchuk on 26.04.17.
+//
+//
+
+#import "OpenVPNTransportStats.h"
+#import "OpenVPNTransportStats+Internal.h"
+
+using namespace openvpn;
+
+@implementation OpenVPNTransportStats
+
+- (instancetype)initWithTransportStats:(ClientAPI::TransportStats)stats
+{
+    self = [super init];
+    if (self) {
+        _bytesIn = stats.bytesIn;
+        _bytesOut = stats.bytesOut;
+        _packetsIn = stats.packetsIn;
+        _packetsOut = stats.packetsOut;
+        _lastPacketReceived = stats.lastPacketReceived;
+    }
+    return self;
+}
+
+@end
